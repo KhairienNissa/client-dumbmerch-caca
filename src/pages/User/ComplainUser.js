a436e16b682f0f127c62c7037985a07a4d35a2b2
KhairienNissa <cacakhairien@gmail.com>
2022-05-14 22:36:35 +0700

    first commit

--- src/pages/User/ComplainUser.js
+++ src/pages/User/ComplainUser.js
@@ -26,7 +26,7 @@ export default function Complain() {
         const title = "Complain by User";
         document.title = "DumbMerch | " + title;
         
-        socket = io('http://localhost:5000', {
+        socket = io('https://dumbmerch-khairien.herokuapp.com/api/v1', {
             auth: {
                 token: localStorage.getItem("token")
             },
